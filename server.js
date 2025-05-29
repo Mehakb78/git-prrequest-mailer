bfc4b2fb6345705b0c094b81fa1bd2c2c43694c8
mehakb7 <mehak@founderandlightning.com>
2025-05-29 16:52:51 +0530

    fixing the port

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ import express from "express";
 import {config} from"./utils/config.js"
 const app = express();
 
+
+const port  = process.env.PORT || config.PORT;
+
 app.use(express.json(()=>{
-    
+
 }));
 
 
 
 
 
-app.listen(config.port,()=>{
+app.listen(config.PORT,()=>{
 
-    console.log("listening in port");
+    console.log("listening in port", port);
 
 },)
